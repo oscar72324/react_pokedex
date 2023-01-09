b8f71a45438619978c6ed5629654d1f13c78df0d
Oscar <Oscaribarra72@hotmail.com>
2023-01-08 21:05:14 -0800

    fixed key issue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,13 +8,13 @@ const Card = (props) => {
         {props.loading ? <p>Loading...</p> 
         : props.pokemon.map(item => {
             return(
-                <>
+
                     <div key={item.id} className={classes.card}>
                         <h2>{item.id}</h2>
                         <img src={item.sprites.front_default} alt="" />
                         <h2>{item.name}</h2>
                     </div>
-                </>
+
         )})
         }
 
